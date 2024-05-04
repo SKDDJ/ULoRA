e45d7c29315dcb32d579620ea64199cb6b1bd6d4
yimingshi <yimingshi666@gmail.com>
2024-05-04 16:28:22 +0800

    add llama2/3 etc

---
+++ utils/down_llama/convert_llama_hf.sh
@@ -0,0 +1,5 @@
+python /root/shiym_proj/Sara/utils/down_llama/convert_llama_weights_to_hf.py \
+    --input_dir /root/shiym_proj/Sara/utils/down_llama/llama3/Meta-Llama-3-8B --model_size 8B --output_dir /root/shiym_proj/Sara/model/llama3/
+
+python /root/shiym_proj/Sara/utils/down_llama/convert_llama_weights_to_hf.py \
+    --input_dir /root/shiym_proj/Sara/utils/down_llama/llama2/llama-2-7b --model_size 7B --output_dir /root/shiym_proj/Sara/model/llama2/
