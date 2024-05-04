53c7b458899f77340c924f7fed9015969f5ee168
yimingshi <yimingshi666@gmail.com>
2024-05-04 22:25:52 +0800

    sync

--- utils/down_llama/convert_llama_hf.sh
+++ utils/down_llama/convert_llama_hf.sh
@@ -1,5 +1,6 @@
-python /root/shiym_proj/Sara/utils/down_llama/convert_llama_weights_to_hf.py \
-    --input_dir /root/shiym_proj/Sara/utils/down_llama/llama3/Meta-Llama-3-8B --model_size 8B --output_dir /root/shiym_proj/Sara/model/llama3/
+# python ~/anaconda3/envs/ldm/lib/python3.8/site-packages/transformers/models/llama/convert_llama_weights_to_hf.py \
+#     --input_dir /root/shiym_proj/Sara/utils/down_llama/llama3/Meta-Llama-3-8B --model_size 8B --output_dir /root/shiym_proj/Sara/model/llama3/
 
-python /root/shiym_proj/Sara/utils/down_llama/convert_llama_weights_to_hf.py \
-    --input_dir /root/shiym_proj/Sara/utils/down_llama/llama2/llama-2-7b --model_size 7B --output_dir /root/shiym_proj/Sara/model/llama2/
+python ~/anaconda3/envs/ldm/lib/python3.8/site-packages/transformers/models/llama/convert_llama_weights_to_hf.py \
+    --input_dir /root/shiym_proj/Sara/utils/down_llama/llama2/ --model_size 7B --output_dir /root/shiym_proj/Sara/model/llama2/
+
