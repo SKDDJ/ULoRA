83df16bf0b73c69537d631e4ea0553e0d89c7722
yimingshi <yimingshi666@gmail.com>
2024-06-03 00:06:15 +0800

    lr ablation and rank ablation

--- vit-finetune/ldu_run_vit_base_sara.sh
+++ vit-finetune/ldu_run_vit_base_sara.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 echo "Starting Evaluations..."
 date
-# | 缩写     | Class名称        |
-# |----------|------------------|
-# | cifar10  | CIFAR10          |
-# | cifar100 | CIFAR100         |
-# | dtd      | DTD              |
-# | cars     | StanfordCars     |
-# | aircraft | FGVCAircraft     |
-# | pets37   | OxfordIIITPet    |
-# | flowers102| Flowers102      |
-# | eurosat  | EuroSAT          |
 
 MODEL_NAME="vit-b16-224-in21k"
 NAME="base"
-# MODEL_NAME="vit-l32-224-in21k" # Assuming a placeholder name for ViT/Large
-# NAME="large"
-# init_method = 'svd'  # 可选项: 'svd', 'uniform', 'normal', 'constant', 'ones', 'zeros'
 INIT_SARA_WEIGHTS="ldu"
 init_sara_weights=ldu
 INIT_METHOD="lu"
@@ -26,13 +13,13 @@ LR_SCHEDULER_NAME=linear
 
 OPT="adamw"
 DATASETS=("cifar10" "cifar100" "dtd" "aircraft"  "eurosat" "pets37" "flowers102" "cars")
-# DATASETS=("flowers102")
-gpu=7
 
-R=768
-ALPHA=768
-FULLRANK="768-1"
-LR=5e-5
+gpu=0
+
+R=1
+ALPHA=1
+FULLRANK="1-1"
+LR=3e-3
 
 SEED=42
 
@@ -40,7 +27,7 @@ for DATASET in "${DATASETS[@]}"; do
     echo "Evaluating on ${DATASET} with ${MODEL_NAME}..."
     CONFIG="/root/shiym_proj/Sara/vit-finetune/configs/sara/${DATASET}.yaml"
     CUDA_VISIBLE_DEVICES=$gpu python main.py fit --config $CONFIG --seed_everything $SEED --trainer.accelerator gpu --trainer.devices=[0] --trainer.precision 16-mixed \
-        --trainer.max_epochs 10 --trainer.logger.name TESTLR-$LR-$init_method-$FULLRANK-$NAME-$DATASET-R-$R-LR-$LR-SCHEDULER-$LR_SCHEDULER_NAME --model.warmup_steps 30 --model.lr $LR --model.scheduler $LR_SCHEDULER --model.optimizer $OPT \
+        --trainer.max_epochs 10 --trainer.logger.name RRR-$R-$init_method-$FULLRANK-$NAME-$DATASET-R-$R-LR-$LR-SCHEDULER-$LR_SCHEDULER_NAME --model.warmup_steps 30 --model.lr $LR --model.scheduler $LR_SCHEDULER --model.optimizer $OPT \
       --data.batch_size 128 --data.dataset $DATASET --data.workers 4 --model.model_name $MODEL_NAME \
       --model.lora_r $R --model.lora_alpha $ALPHA --model.init_method $INIT_METHOD --model.init_sara_weights $INIT_SARA_WEIGHTS > ./logs/61-${INIT_METHOD}/vit_base_${DATASET}.log 2>&1
     
